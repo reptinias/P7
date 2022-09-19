6aa844b35810c638efce5a0b04348c991c6074c9
reptinias <k.spar@hotmail.com>
2022-09-19 15:00:04 +0200

    First push

---
+++ GestureDNN/classifier.py
@@ -0,0 +1,56 @@
+import csv
+
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+RANDOM_SEED = 42
+
+dataset = 'file.csv'
+model_save_path = 'keypoint_classifier.hdf5'
+
+NUM_CLASSES = 3
+
+X_dataset = np.loadtxt(dataset, delimiter=',', dtype='float32', usecols=list(range(1, (21 * 3) + 1)))
+y_dataset = np.loadtxt(dataset, delimiter=',', dtype='int32', usecols=(0))
+X_train, X_test, y_train, y_test = train_test_split(X_dataset, y_dataset, train_size=0.75, random_state=RANDOM_SEED)
+
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Input(shape=(21 * 3,)),
+    tf.keras.layers.Dropout(0.1),
+    tf.keras.layers.Dense(30, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(20, activation='relu'),
+    tf.keras.layers.Dropout(0.4),
+    tf.keras.layers.Dense(10, activation='relu'),
+    tf.keras.layers.Dense(NUM_CLASSES, activation='softmax')
+])
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    model_save_path, verbose=1, save_weights_only=False)
+
+es_callback = tf.keras.callbacks.EarlyStopping(patience=20, verbose=1)
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(
+    X_train,
+    y_train,
+    epochs=1000,
+    batch_size=128,
+    validation_data=(X_test, y_test),
+    callbacks=[cp_callback, es_callback]
+)
+
+val_loss, val_acc = model.evaluate(X_test, y_test, batch_size=128)
+
+model = tf.keras.models.load_model(model_save_path)
+
+predict_result = model.predict(np.array([X_test[0]]))
+print(np.squeeze(predict_result))
+print(np.argmax(np.squeeze(predict_result)))
